ed9d0fb81a3e48f354baa6b4c30ca81e2a8735b9
Suprith Singh <123349613+suprith03@users.noreply.github.com>
2025-12-24 22:15:33 +0530

    backend implement

---
+++ src/server.js
@@ -0,0 +1,8 @@
+import http from "http"
+import app from "./app.js"
+import { initSocket } from "./sockets/socket.js"
+
+const server = http.createServer(app)
+initSocket(server)
+
+server.listen(process.env.PORT)
